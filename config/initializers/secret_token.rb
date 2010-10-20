f3479f153311ce5ead89cec4a89af69e2f61e4fb
David James <davidcjames@gmail.com>
2010-10-20 10:46:50 -0400

    rails new bdad -d postgresql -J -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bdad::Application.config.secret_token = '9d597c99c4542dcdfe903e04d5eb631cd6336326cd471340df3f38154db867e6f87301b30ce9c143015ac9f372ab10cd7c26a926495f851e86323a1989454e54'
