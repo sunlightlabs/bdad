f5f7d873abcd7e1b208c1f23d05a00eb212248b4
David James <djames@sunlightfoundation.com>
2010-11-01 22:46:14 -0400

    Lazily load the gallery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,3 +10,22 @@ $('document').ready(function() {
       $(this).removeClass('pressed');
     });
 });
+
+$(window).load(function() {
+  var items = [];
+  $('#gallery > div.sketch > div.container').each(function(index) {
+    items.push(this);
+  });
+  var load = function() {
+    var item = items.shift();
+    if(item) {
+      var $item = $(item);
+      var $img = $item.find('img.loading')
+      url = $img.attr('data-url');
+      console.log('%s', url);
+      $img.replaceWith('<object data="' + url +'" type="image/svg+xml">');
+      setTimeout(load, 500);
+    }
+  };
+  setTimeout(load, 500);
+});
